a63aa3219e89cf2649028210daa13b1a1f54db17
Jake Shams <jshams62@gmail.com>
2019-07-30 10:23:16 -0700

    run this file to see my graph

--- Graph-Tutorial/friends.py
+++ Graph-Tutorial/friends.py
@@ -2,12 +2,13 @@ from graph import Graph
 
 if __name__ == '__main__':
     friends_graph = Graph('friends.txt')
-    print(f'# Edges : {friends_graph.edge_count}')
-    print(f'# Vertices : {friends_graph.vertex_count}')
-    print(f'Verticies : \n{", ".join(friends_graph.get_vertices())}')
+    print('GRAPH OF MY FRIENDS')
+    print(f'Number of edges : {friends_graph.edge_count}')
+    print(f'Number of vertices : {friends_graph.vertex_count}')
+    print(f'Verticies : {", ".join(friends_graph.get_vertices())}')
     bailey_to_dan = friends_graph.find_shortest_path('Bailey', 'Daniel')
     print(
         f'Shortest path from Bailey to Daniel : {" -> ".join(bailey_to_dan)}')
-    print(f'# Edges from Bailey to Daniel : {len(bailey_to_dan) - 1}')
+    print(f'Number of edges from Bailey to Daniel : {len(bailey_to_dan) - 1}')
     print(f'Random clique : {", ".join(list(friends_graph.clique()))}')
     print(f'Diameter of friends : {friends_graph.diameter()}')
